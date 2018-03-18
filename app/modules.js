2db1d61edcd17f725fe200d3fdce27655bd9cb91
jfipps <jfipps1995@csu.fullerton.edu>
2018-03-17 18:22:38 -0700

    player modules

---
+++ app/modules.js
@@ -0,0 +1,83 @@
+var sqlite3 =require('sqlite3').verbose();
+var db = new sqlite3.Database(data.db);
+
+//Functions to get player info by team name
+
+exports.getPlayerName = function (var teamName) {
+	var names = [];
+	db.serialize(function () {
+		db.each("SELECT name FROM players WHERE team="+teamName, function(err, row) {
+			names.push(row.name);
+		});
+	});
+	return names;
+};
+
+exports.getPlayerNumber = function(var playerName) {
+	var num;
+	db.serialize(function () {
+		db.each("SELECT num FROM playeres WHERE name="+playerName), function(err, row) {
+			num = row.num;
+		});
+	});
+	return num;
+};
+
+exports.getPlayerPosition = function(var playerName) {
+	var pos;
+	db.serialize(function () {
+		db.each("SELECT pos FROM players WHERE name="+playerName), function(err, row) {
+			pos = row.pos;
+		});
+	});
+	return pos;
+};	
+
+exports.getPlayerHeight = function(var playerName) {
+	var height;
+	db.serialize(function () {
+		db.each("SELECT height FROM players WHERE name="+playerName), function(err, row) {
+			height = row.height;
+		});
+	});
+	return height;
+};
+
+exports.getPlayerWeight = function(var playerName) {
+	var weight;
+	db.serialize(function () {
+		db.each("SELECT weight FROM players WHERE name="+playerName), function(err, row) {
+			weight = row.weight;
+		});
+	});
+	return weight;
+};
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
